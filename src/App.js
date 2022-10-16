010336220aa9d111073b1150added66d2a2e4647
이용훈 <dydrkfl7400@naver.com>
2022-10-16 21:24:55 +0900

    2022/10/16

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Button, Card, ListGroup } from 'react-bootstrap';
+import korea from './data'
+
 
 function App() {
+
+  let [ account, setAccount ] = useState(false);
+  let [ i, setI ] = useState('');
+  let [ fade, setFade ] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main-img'>
+        {
+          korea.map((a, i) => {
+            return (
+              <div className={`main-name main-name-${i}`} onMouseOver={()=>{
+                let timer = setTimeout(()=>{setFade('card-focus')}, 100);
+                setAccount(true);
+                setI(a.id);
+              }} onMouseOut={()=>{
+                setAccount(false);
+                setFade('');
+              }}>{a.name}</div>
+            );
+          })
+        }
+
+        {
+          account == true ? <Account korea = { korea } i = { i } fade = { fade } /> : null
+        }
+
+      </div>
     </div>
+  );
+}
+
+function Account(props) {
+
+  return (
+    <>
+      <div className={`card ${props.fade} card-${props.korea[props.i].id} scroll`}>
+        <div>
+          <h3>{props.korea[props.i].name} <span>{props.korea[props.i].eng_name}</span></h3>
+          <h4>도시정보</h4>
+          <h5>설명</h5>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,113 @@
+let korea = [
+
+    {
+        id : 0,
+        name : "강원도",
+        eng_name : "Gangwon-do"
+    },
+
+    {
+        id : 1,
+        name : "서울",
+        eng_name : "Seoul"
+    },
+
+    {
+        id : 2,
+        name : "인천",
+        eng_name : "Incheon"
+    },
+
+    {
+        id : 3,
+        name : "경기도",
+        eng_name : "Gyenggi-do"
+    },
+
+    {
+        id : 4,
+        name : "충청남도",
+        eng_name : "Chungcheongnam-do"
+    },
+
+    {
+        id : 5,
+        name : "대전",
+        eng_name : "Daejeon"
+    },
+
+    {
+        id : 6,
+        name : "충청북도",
+        eng_name : "Chungcheongbuk-do"
+    },
+
+    {
+        id : 7,
+        name : "경상북도",
+        eng_name : "Gyeongsangbuk-do"
+    },
+
+    {
+        id : 8,
+        name : "대구",
+        eng_name : "Daegu"
+    },
+
+    {
+        id : 9,
+        name : "전라북도",
+        eng_name : "Jeollabuk-do"
+    },
+
+    {
+        id : 10,
+        name : "전라남도",
+        eng_name : "Jeollanam-do"
+    },
+
+    {
+        id : 11,
+        name : "광주",
+        eng_name : "Gwangju"
+    },
+
+    {
+        id : 12,
+        name : "경상남도",
+        eng_name : "Gyeongsangnam-do"
+    },
+
+    {
+        id : 13,
+        name : "부산",
+        eng_name : "Busan"
+    },
+
+    {
+        id : 14,
+        name : "울산",
+        eng_name : "Ulsan"
+    },
+
+    {
+        id : 15,
+        name : "제주도",
+        eng_name : "Jeju"
+    },
+
+    {
+        id : 16,
+        name : "울릉도",
+        eng_name : "Ulleung-gun"
+    },
+
+    {
+        id : 17,
+        name : "독도",
+        eng_name : "Dokdo"
+    }
+
+]
+
+export default korea;
